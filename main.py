1a6d37c51d02cd0ff4eccefb018ec5356c37a297
Sophia Lis <h11d3nname@gmail.com>
2024-01-06 08:38:42 +0300

    feat: add insertion sort

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from recursion import print_nums
 print_nums(10)
 
 
-from binary_search import search_number, search_word
+from binary_search import *
 
 my_list_nums = [3,14,22,24,41,65,78,99]
 my_list_words = ['apple', 'blueberry', 'chicken', 'egg', 'meat', 'pasta', 'pork', 'watermelon']
 
 print(search_number(my_list_nums, 65))
-print(search_number([3,14,22,24,41,65,78,99], 45))
+print(search_number(my_list_nums, 45))
 
 print(search_word(my_list_words, 'blueberry'))
 print(search_word(my_list_words, 'fish'))
 
 
-from sort import bubble_sort
+from sort import *
 
 my_list_unsorted_nums = [1,5,3,24,21,54,32,11,12]
 
 print(bubble_sort(my_list_unsorted_nums))
+print(insertion_sort(my_list_unsorted_nums))
--- sort.py
+++ sort.py
@@ -13,3 +13,15 @@ def bubble_sort(a_list):
             return a_list
         
     return a_list
+
+def insertion_sort(a_list):
+    for i in range(1, len(a_list)):
+        value = a_list[i]
+
+        while i > 0 and a_list[i - 1] > value:
+            a_list[i] = a_list[i - 1]
+            i = i - 1
+
+        a_list[i] = value
+
+    return a_list
